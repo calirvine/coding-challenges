6c75b0d44bc2cc587018a97ce3b6bd8e5181a610
Cal Irvine <cirvine@Administrators-MacBook-Pro.local>
2020-01-14 11:47:31 -0500

    added prettier config then ran prettier to make everything... prettier.

--- 3Sum.js
+++ 3Sum.js
@@ -6,14 +6,14 @@
 //goal find all triplets that sums to 0
 //only include unique triplets
 var threeSum = function(nums) {
-  if (nums.length < 3) return [];
-  nums = nums.sort((a, b) => a - b);
+  if (nums.length < 3) return []
 
-  let numsDict = {};
+  let numsDict = {}
   for (let i of nums) {
-    numsDict[i] ? numsDict[i]++ : (numsDict[i] = 1);
+    numsDict[i] ? numsDict[i]++ : (numsDict[i] = 1)
   }
-};
+}
 
-const input = [-1, 0, 1, 2, -1, -4];
-const result = threeSum(input);
+const input = [-1, 0, 1, 2, -1, -4]
+const result = threeSum(input)
+console.log(result)
